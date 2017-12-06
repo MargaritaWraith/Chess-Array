054fd2727b904b6b7bdf8dee8748fc81732c2504
Margarita Wraith <Kondratieff@live.ru>
2017-12-06 18:56:40 +0300

    Добавьте файлы проекта.

---
+++ Chess Array/HeaderArray.h
@@ -0,0 +1,8 @@
+#ifndef _ARRAY
+#define	_ARRAY
+using namespace std;
+
+void ChessArray();
+void PerimetrArray();
+
+#endif // !_ARRAY
---
+++ Chess Array/MainProgram.cpp
@@ -0,0 +1,15 @@
+#include "stdafx.h"
+#include <iostream>
+#include "HeaderArray.h"
+using namespace std;
+
+int main()
+{
+	//ChessArray();
+
+	PerimetrArray();
+
+	cin.get();
+	cin.get();
+	return 0;
+}
